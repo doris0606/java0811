121a87b9ac03760624c5b24440f0a890360172b4
MB-study <MB-study@192.168.1.143>
2020-08-11 13:38:57 +0800

    java

---
+++ src/main/java/com/pcschool/ocp/d01/ArrayTest.java
@@ -0,0 +1,14 @@
+package com.pcschool.ocp.d01;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] scores = {100, 90, 80, 70};
+        System.out.println(scores[0]);
+        System.out.println(scores[1]);
+        System.out.println(scores[2]);
+        System.out.println(scores[3]);
+        System.out.println(scores.length);
+    }
+    
+    
+}

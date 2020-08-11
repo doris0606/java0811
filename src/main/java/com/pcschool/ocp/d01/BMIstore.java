98074a6cf313a66415fe2d0299bae700580d6995
MB-study <MB-study@192.168.1.143>
2020-08-11 11:25:21 +0800

    java

--- src/main/java/com/pcschool/ocp/d01/BMIstore.java
+++ src/main/java/com/pcschool/ocp/d01/BMIstore.java
@@ -1,19 +1,25 @@
 
 package com.pcschool.ocp.d01;
 
+import java.awt.PageAttributes;
+import java.io.PrintStream;
+
 
 public class BMIstore {
     public static void main(String[] args) {
         BMI bl = new BMI();
         bl.name="John";bl.h=170;bl.w=60;
-        bl.bmi = bl.calcBMI();
+        bl.setBMI();
         
         BMI b2 = new BMI();
         b2.name="Mary";b2.h=165;b2.w=45;
-        b2.bmi = b2.calcBMI();
+        b2.setBMI();
                
-        
-        System.out.printf("%s bmi =%");
+    
+       System.out.printf
+        ("%s (%.1f, %.1f)bmi =%.2f\n", b1.name, b1.h, b1.w, b1.bmi);
+       System.out.printf
+        ("%s (%.1f, %.1f)bmi =%.2f\n", b2.name, b2.h, b2.w, b2.bmi);
 
        
     

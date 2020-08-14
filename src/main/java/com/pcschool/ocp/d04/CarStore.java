05a2b2b7a5922af4dd232e82474c7b15486ddbab
MB-study <MB-study@192.168.1.143>
2020-08-14 11:49:19 +0800

    java

---
+++ src/main/java/com/pcschool/ocp/d04/CarStore.java
@@ -0,0 +1,11 @@
+package com.pcschool.ocp.d04;
+
+public class CarStore {
+    public static void main(String[] args) {
+        Car.printCar2();
+        Car car = new Car();
+        Car.printCar();
+        Car.printCar3();
+    }
+    
+}
---
+++ src/main/java/com/pcschool/ocp/d04/ConstructorDemo5.java
@@ -0,0 +1,12 @@
+package com.pcschool.ocp.d04;
+
+
+public class ConstructorDemo5 {
+    public static void main(String[] args) {
+        Food f1 = new Food();
+        Food f2 = new Food300);
+        System.out.println(f1);
+        System.out.println(f2);
+    }
+    
+}

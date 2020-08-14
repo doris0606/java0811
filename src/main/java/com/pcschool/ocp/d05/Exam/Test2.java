8e9727dfac07ef363be3512ec5f9a81d0f39166a
MB-study <MB-study@192.168.1.143>
2020-08-14 15:47:21 +0800

    java

---
+++ src/main/java/com/pcschool/ocp/d05/Exam/Test2.java
@@ -0,0 +1,18 @@
+
+package com.pcschool.ocp.d05.Exam;
+
+public class Test2 {
+    String s;
+    public Test2(String s) {
+        this.s = s.concat("8");
+    } 
+    public static void main(String[] args) {
+        String s  = "Java";
+        Test2 t2 = new Test2(s);
+        System.out.println(s);
+        System.out.println(t2.s);
+    }
+            
+            
+    
+}

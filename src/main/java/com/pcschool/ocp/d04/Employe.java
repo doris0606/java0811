4c70eafb71c045e5e7fdec696dc0dfd6bd3e3c17
MB-study <MB-study@192.168.1.143>
2020-08-14 09:54:20 +0800

    java

---
+++ src/main/java/com/pcschool/ocp/d04/Employe.java
@@ -0,0 +1,31 @@
+
+package com.pcschool.ocp.d04;
+
+public class Employe {
+    private String name;
+    private int salary;
+   
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    
+    public Employee {
+        this.name = name;
+        this,salary = salary;
+    }
+    public Employee(){
+        name = "Unknow";
+        salary = 1000
+             
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return Employee(St)
+    }
+    
+}
---
+++ src/main/java/com/pcschool/ocp/d04/Pen.java
@@ -0,0 +1,20 @@
+package com.pcschool.ocp.d04;
+
+public class Pen {
+
+    private int price;
+    
+   
+
+    public Pen(int price) {
+        if (price < 10) {
+            this.price = 10;
+        } else {
+            this.price = price;
+        }
+    }
+    
+    public Pen() {
+        this(5);
+    }
+}

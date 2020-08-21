2ee0ed6a42f9cd3bc014a11b0b856e8365cd94bd
MB-study <MB-study@192.168.1.143>
2020-08-21 11:23:47 +0800

    java

---
+++ src/main/java/com/pcschool/ocp/d01/d10/case3/SetDemo3.java
@@ -0,0 +1,27 @@
+
+package com.pcschool.ocp.d01.d10.case3;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetDemo3 {
+    public static void main(String[] args) {
+        Set<Integer> set = new LinkedHashSet<>();
+        set.add(100);
+        set.add(90);
+        set.add(80);
+        System.out.println(set);
+        Iterator<Integer> iter = set.iterator();
+        int sum = 0;
+        while (iter.hasNext()) {
+            Integer next = iter.next();
+            sum += next;
+            
+        }
+        System.out.println(sum);
+        
+        sum = set.stream();
+    }
+    
+}

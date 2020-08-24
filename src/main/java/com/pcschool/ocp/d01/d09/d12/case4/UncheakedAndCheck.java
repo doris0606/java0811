c9082919337e6d33e1530d9d921ecb70f2183a59
MB-study <MB-study@192.168.1.143>
2020-08-24 14:19:49 +0800

    java

--- src/main/java/com/pcschool/ocp/d01/d09/d12/case4/UncheakedAndCheck.java
+++ src/main/java/com/pcschool/ocp/d01/d09/d12/case4/UncheakedAndCheck.java
@@ -1,12 +1,22 @@
 package com.pcschool.ocp.d01.d09.d12.case4;
 
+import static com.pcschool.ocp.d01.d09.d12.case4.UncheakedAndCheck.d;
+import java.io.EOFException;
+
 public class UncheakedAndCheck {
     public static void main(String[] args) {
-        
+        a(0);
+        b(0);
+        try {
+            c(0);
+        }catch (ArithmeticException e) {
+            System.out.printf("main 自行處理數學錯誤: %s\n", e);
+        }
+     
+       
     }
-    public static void a() {
+    public static void a(int d) {
         int x = 10;
-        int d = 0;
         try {
             int result = x / d;
             System.out.println(result);
@@ -23,4 +33,18 @@ public class UncheakedAndCheck {
         int result = x / d;
         System.out.println(result);
     }
-}
+    public static void c(int d) {
+        int x = 10;
+        int result = x / d;
+        System.out.println(result);
+    }
+     public static void d(int d) {
+        int x = 10;
+        if(d == 0) {
+            Exception e = new EOFException("d 說: 分母不可為 0");
+        }
+    
+
+    
+    }
+    }
